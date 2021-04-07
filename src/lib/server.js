f505e4b4fd1cc3b9d3eafbbb2ab86883eea0ada1
Marvin Isaac <misaac@pushnami.com>
2021-04-07 22:51:44 +0000

    :recycle: Set up project for testing
    By separating init() and start() functions, API could be tested without actually starting a server

---
+++ src/lib/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: '0.0.0.0'
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello/',
+    handler: (request, h) => {
+        return 'Hello, world!';
+    }
+});
+
+exports.init = async () => {
+    await server.initialize();
+    return server;
+};
+
+exports.start = async () => {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+    return server;
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
